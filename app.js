1165140e1f61de2fc892f9990e0990cc779c1f87
levent <lvnt4834celebi@gmail.com>
2023-01-16 12:47:52 +0300

    start button added

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 let screen = document.querySelector("#sayac")
-
+let buton = document.getElementById("btn")
+buton.addEventListener("click",()=>{
+    window.location.reload()
+})
 const click=()=>{
-    
-
-  
-   let minute =
-   10;
+    let minute = 10;
    const x = setInterval(() => {
     screen.innerHTML = `<span> ${minute} </span>`;
      minute--;
     if (minute ==0) {
       clearInterval(x);
       screen.innerHTML = `<span> Let's Go! </span>`;
-      
-      
-
-    } 
-    if(minute==0){
-        
-    }
-
-   
-   }, 1000);
+   } 
+ }, 1000);
 }
 click()
+
 
